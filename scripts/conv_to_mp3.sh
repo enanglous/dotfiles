e413698d5e9aa50693bda1661304f7dd5cb76b54
enanglous <idiswaddis@gmail.com>
2025-07-05 00:42:46 +0500

    make script usable for other file formats

--- scripts/conv_to_mp3.sh
+++ scripts/conv_to_mp3.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 for file in *;
 do
-  if [[ "$file" == *.m4a || "$file" == *.aac || "$file" == *.opus ]]; then
+  if [[ "$file" == *.m4a ]]; then
     ffmpeg -i "$file" -acodec libmp3lame -ab 256k "${file%.m4a}.mp3"
+  elif [[ "$file" == *.aac ]]; then
+    ffmpeg -i "$file" -acodec libmp3lame -ab 256k "${file%.aac}.mp3"
+  elif [[ "$file" == *.opus ]]; then
+    ffmpeg -i "$file" -acodec libmp3lame -ab 256k "${file%.opus}.mp3"
+  elif [[ "$file" == *.mp4 ]]; then
+    ffmpeg -i "$file" -acodec libmp3lame -ab 256k "${file%.mp4}.mp3"
   fi
 done
